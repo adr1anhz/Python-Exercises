20bede67247603fc55f3a18217f336c2cef67e61
adr1anhz <addriaan@proton.me>
2025-07-10 11:18:07 +0200

    Add load data from database.db

--- SQLite-student-management-system/main.py
+++ SQLite-student-management-system/main.py
@@ -1,6 +1,7 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget,QTableWidgetItem
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -21,11 +22,20 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
 
     def load_data(self):
-        self.table
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                print(row_data)
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
 
 
 
@@ -35,7 +45,8 @@ class MainWindow(QMainWindow):
 
 
 app = QApplication(sys.argv)
-calculator = MainWindow()
-calculator.show()
+age_calculator = MainWindow()
+age_calculator.show()
+age_calculator.load_data()
 sys.exit(app.exec())
 
