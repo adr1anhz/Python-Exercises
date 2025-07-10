21cca715c7cd6415c48d63fb274e95eb3aa23f8f
adr1anhz <addriaan@proton.me>
2025-07-10 10:59:14 +0200

    Add Table Columns

--- SQLite-student-management-system/main.py
+++ SQLite-student-management-system/main.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget
 from PyQt6.QtGui import QAction
 import sys
 
@@ -18,6 +18,14 @@ class MainWindow(QMainWindow):
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
 
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.setCentralWidget(self.table)
+
+
+    def load_data(self):
+        self.table
 
 
 
